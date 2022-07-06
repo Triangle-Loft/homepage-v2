86b2ddd93139d4cfda1d308a2bd2ba4ded40f239
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-07-06 19:14:56 -0400

    add team

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,16 +100,11 @@ Triangle Loft; designed by humans with humans in mind.
             <p className={styles.titleDescription}>Our People</p>
             <h2 className={styles.title}>The Team</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-              eu nibh viverra, cursus eros non, vestibulum sem. Sed tincidunt
-              posuere gravida. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Nulla iaculis turpis nisi, non feugiat lacus
-              dignissim ac. Donec bibendum accumsan felis convallis varius.
-              Donec cursus sem id purus sodales aliquet. Donec convallis lacus
-              arcu, mattis efficitur lectus pellentesque quis. Aliquam
-              vestibulum fringilla dolor. Sed sed sapien pretium, bibendum orci
-              sed, vulputate lorem. Sed a lacus sodales, vehicula libero
-              ullamcorper, euismod mi.
+            The Triangle Loft team is like our space, filled with creativity and exclusively inclusive to all. They are flexible in their ways, customer centric, and like each of us, possess their own unique personalities superbly meshing with our brands philosophy. From the front door to sanitation, we have put together the perfect team which has come to be one of the greatest characteristics of our space.
+
+Our Mixologists are the designers and developers of cocktails, mocktails and more. Each are like scientists who experiment with spirits and flavors. They use beakers to hold their syrups and flasks for their infusions, as they continually look for new ways to create the next trend in cocktails.
+
+The serving team is first in class and come from all areas of life. They are actors, artists, students, and parents. Some of them are feisty while others subdued. But don’t let that fool you, they are the best of the best in all they do and are aware of the role they play at each event.
             </p>
           </div>
           <div></div>
